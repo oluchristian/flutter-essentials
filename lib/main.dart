f8fe8408320b81b711d475de039f2d3ed09f6d38
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-06 14:07:25 +0100

    chapter 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,56 +2,112 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: const HomePage(),
     theme: ThemeData(
       primarySwatch: Colors.purple,
     ),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Awesome App')),
-      body: Container(
-        width: 300,
-        height: 400,
-        color: Colors.teal,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8),
-              
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text(
+          'Awesome App',
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(
+            16.0,
+          ),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/bg.jpg',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                      SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter some text",
+                        labelText: "Name",
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-              padding: EdgeInsets.all(8),
-              
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              alignment: Alignment.center,
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Ughagwu Oluchukwu'),
+              accountEmail: Text('oluchristian98@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z3V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'),
               ),
-              Container(
-              padding: EdgeInsets.all(8),
-              
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person,
               ),
+              title: Text('Ughagwu Oluchukwu'),
+              subtitle: Text('Developer'),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.email,
+              ),
+              title: Text('Email'),
+              subtitle: Text('oluchristian98@gmail.com'),
+              trailing: Icon(Icons.edit),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+        },
+        child: const Icon(Icons.send),
       ),
     );
   }
 }
-
